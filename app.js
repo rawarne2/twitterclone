c043c91052ee1dd566455baaca29e883e185cce8
Victor Barrancos <victorbarrancos@gmail.com>
2018-03-05 16:52:29 -0600

    update

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  //console.log(Object.keys(req))
+  console.log(req.method + " " + req.url);
+  next();
+})
+
+app.use("/special*", (req, res, next) => {
+  console.log("You reached the special area.");
+  next();
+})
+
+app.get("/news", (req, res, next) => {
+  res.send("Twitter news");
+})
+
 app.get("/", (req, res, next) => {
   res.send("Welcome to twitter");
 })
